505eae7cfeca7ac2a3c5fc19579384bd33dd8d7e
Anav Chand <anavkki@gmail.com>
2023-06-27 10:04:09 +1200

    Fecthing Data

--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react"
+
 function Home() {
+    const [workouts, setWorkouts] = useState(null)
+    useEffect(() => {
+        async function fetchWorkouts() {
+            const response = await fetch("/api/workouts")
+            const data = await response.json()
+            // response.ok ? console.log(data) : console.log("Fetch Error")
+            console.log(data)
+        }
+        fetchWorkouts() 
+    }, [])
+
     return(
-        <div className="hom ">
-            <h1>Home Page</h1>
+        <div className="home">
+            <div>
+                {/* {workouts.map((workout) => (<p key={workout._id}>{workout.title}</p>))} */}
+            </div>
         </div>
     )
 }
