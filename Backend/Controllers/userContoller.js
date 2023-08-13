b5ac21464156add4c3a4dbebd41f1d122f1ecfd0
Anav Chand <anavkki@gmail.com>
2023-08-13 18:56:26 +1200

    happy anav :)

--- Backend/Controllers/userContoller.js
+++ Backend/Controllers/userContoller.js
@@ -1,16 +1,33 @@
 const User = require("../Models/userModel")
+const jwt = require("jsonwebtoken")
+
+// json web token creator
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+}
 
 // login user
 const loginUser = async(req, res) => {
-    res.json({message: "login user"})
+    const { email, password } = req.body
+    try {
+        const user = await User.login(email, password)
+        // create jwt token
+        const token = createToken(user._id)
+        res.status(200).json({email, token})
+    }
+    catch(err){
+        res.status(400).json({Error: err.message})
+    }
 }
 
-// sign in user
+// sign up user
 const signupUser = async(req, res) => {
     const { email, password } = req.body
     try {
         const user = await User.signup(email, password)
-        res.status(200).json({email, user})
+        // create jwt token
+        const token = createToken(user._id)
+        res.status(200).json({email, token})
     }
     catch(err){
         res.status(400).json({Error: err.message})
--- Backend/Models/userModel.js
+++ Backend/Models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose  = require("mongoose")
 const bcrypt = require("bcrypt")
+const validator = require("validator")
 
 const Schema  = mongoose.Schema
 
@@ -17,6 +18,18 @@ const userSchema = new Schema({
 
 // static signup method
 userSchema.statics.signup = async function(email, password) {
+
+    // validation {
+    if(!email || !password){
+        throw Error("Missing Email or Password")
+    }
+    if(!validator.isEmail(email)){
+        throw Error("Email is not valid")
+    }
+    if(!validator.isStrongPassword(password)){
+        throw Error("Password is not strong enough")
+    }
+
     const exists = await this.findOne({ email })
     if (exists) {
         throw Error("Email already in use")
@@ -26,6 +39,25 @@ userSchema.statics.signup = async function(email, password) {
     const hash = await bcrypt.hash(password, salt)
 
     const user = await this.create({ email, password: hash})
+    return user
+}
+
+// static login method
+userSchema.statics.login = async function(email, password){
+    if (!email || !password){
+        throw Error("Missing Email or Password")    
+    }
+
+    const user = await this.findOne({ email })
+    if (!user) {
+        throw Error("Incorrect Email")
+    }
+    
+    const match = await bcrypt.compare(password, user.password)
+    if(!match){
+        throw Error("Incorrct Password")
+    }
+
     return user
 }
 
